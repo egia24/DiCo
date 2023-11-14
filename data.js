56481356ac2af2b7ed6f322588445d0d24d1f8f5
Egia suranta <egiasuranta24>
2023-11-14 18:07:22 +0700

    [BAB 3] State dan Props

---
+++ data.js
@@ -0,0 +1,86 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Product',
+    },
+    {
+      id: 2,
+      categoryName: 'Event',
+    },
+    {
+      id: 3,
+      categoryName: 'Tutorial',
+    },
+    {
+      id: 4,
+      categoryName: 'Sports History',
+    },
+    {
+      id: 5,
+      categoryName: 'Contact',
+    },
+  ];
+  
+  export const BlogList = [
+    {
+      id: 1,
+      title: 'Ada banyak peralatan olahraga sepak bola yang dapat kamu beli agar permainan kamu lebih baik',
+      category: 'Sepak Bola ',
+      image:'https://cdn.pixabay.com/photo/2018/06/12/20/17/soccer-3471402_1280.jpg',
+    },
+    {
+      id: 2,
+      title: 'Disini petunjuk peralatan olahraga badminton yang wajib kamu beli',
+      category: 'Badminton',
+      image: 'https://cdn.pixabay.com/photo/2016/05/31/23/21/badminton-1428046_1280.jpg',
+    },
+    {
+      id: 3,
+      title: 'Disini terdapat rekomendasi peralatan tenis dari berbagai merek',
+      category: 'Tennis',
+      image: 'https://cdn.pixabay.com/photo/2021/08/09/14/56/tennis-6533589_1280.jpg',
+    },
+    {
+      id: 4,
+      title: 'Ada banyak jenis merek bola, pakaian dan aksesoris basket',
+      category: 'Basket',
+      image: 'https://cdn.pixabay.com/photo/2016/11/18/22/10/man-1837119_1280.jpg',
+    },
+    {
+      id: 5,
+      title: 'Beragam merek bola voli yang bisa menjadi referensi sebelum membeli',
+      category: 'Voli',
+      image: 'https://cdn.pixabay.com/photo/2021/07/21/20/11/beach-volleyball-6483905_1280.jpg',
+    },
+    {
+      id: 6,
+      category: 'Badminton',
+      image: 'https://marketplace.canva.com/EAFn2B---Eg/1/0/1131w/canva-merah-dan-hijau-ilustrasi-badminton-rutin-a4-yCRLF3E-Qtw.jpg',
+      createdAt: '28 Aug, 2023',
+    },
+    {
+      id: 7,
+      category: 'Basket',
+      image: 'https://img.freepik.com/free-vector/colored-vintage-basketball-championship-poster_1284-39316.jpg',
+      createdAt: '24 Aug, 2023',
+    },
+    {
+      id: 8,
+      category: 'Tennis',
+      image: 'https://i.pinimg.com/736x/9b/ba/13/9bba1341fe2e63cb44ae8640e81bd642.jpg',
+      createdAt: '15 March, 2023',
+    },
+    {
+      id: 9,
+      category: 'Voli',
+      image: 'https://marketplace.canva.com/EADkSH2NGg8/1/0/1131w/canva-biru-dan-kuning-turnamen-bola-voli-poster-2WLxudAYV04.jpg',
+      createdAt: '8 Aug, 2019',
+    },
+    {
+      id: 10,
+      category: 'Sepak Bola',
+      image: 'https://marketplace.canva.com/EAFrsqv6wps/1/0/1131w/canva-kuning-hijau-simpel-ilustrasi-kompetisi-futsal-poster-7X4ud5Cbxqw.jpg',
+      createdAt: '28 Mei, 2023',
+    },
+  ];
+  
